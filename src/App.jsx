5655ac0efe5aa05ef4244eb827a0a413a53dc596
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-05-22 20:21:54 -0500

    Add container for job recomentation cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
 import './styles/App.scss';
+import { useState } from 'react';
 import Bubble from './components/Bubble/Bubble';
 import ChatLayout from './components/Chat/ChatLayout';
 import { MessageProvider } from './context/message';
@@ -13,8 +13,11 @@ export default function App() {
 
   return (
     <MessageProvider>
-      <main className='container'>
-        <section className='bubble-wrapper'>
+      <main className="container">
+        <div className="results">
+
+        </div>
+        <section className="bubble-wrapper">
           <ChatLayout openChatBubble={toggleChatBubble} />
           <Bubble
             handleToggleBubble={handleToggleBubble}
