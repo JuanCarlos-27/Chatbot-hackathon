a5c20f21eb48a13f53cc17eb4d60c870cce5e058
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-05-22 20:21:17 -0500

    Fix min chat initial

--- src/components/Chat/ChatLayout.jsx
+++ src/components/Chat/ChatLayout.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './chat.scss';
 import InputMessage from './InputMessage';
 import useMessage from '../../hooks/useMessage';
+import { CloseIcon } from '../icons';
 
 export default function ChatLayout({ openChatBubble }) {
   const [error, setError] = useState(false);
@@ -12,39 +13,55 @@ export default function ChatLayout({ openChatBubble }) {
   const handleBotTyping = (state) => setBotTyping(state);
 
   return (
-    <div className={`chat-wrapper ${openChatBubble ? 'opened' : ''}`}>
-      <div className='header-wrapper'>
-        <div className='header-wrapper-title'>Chatbot Infojobs 🤖</div>
+    <div className={`chat-wrapper ${openChatBubble ? 'opened' : 'min'}`}>
+      {!openChatBubble && <CloseIcon id="icon-close-min" />}
+      <div className="header-wrapper">
+        <div className="header-wrapper-title">Chatbot Infojobs 🤖</div>
       </div>
       {error && (
-        <div className='error'>Ha ocurrido un error, vuelve a intentarlo</div>
+        <div className="error">Ha ocurrido un error, vuelve a intentarlo</div>
       )}
-
-      <div className='messages-list-wrapper'>
-        <div className='message-list'>
-          {messageState.map((message) => {
-            const { message: text, id, rol } = message;
-            if (rol === 'bot') {
+      {
+        messageState.length !== 0 &&
+        <div className="messages-list-wrapper">
+          <div className="message-list">
+            {messageState.map((message) => {
+              const { message: text, id, rol } = message;
               return <Message key={id} message={text} rol={rol} />;
-            }
-            return <Message key={id} message={text} rol={rol} />;
-          })}
-          {botTyping && <BotTyping />}
+            })}
+            {botTyping && <BotTyping />}
+          </div>
         </div>
-      </div>
+      }
       <InputMessage onError={handleError} onTyping={handleBotTyping} />
     </div>
   );
 }
+
+// function Chips({ chips }) {
+//   return (
+//     <div className="chips-wrapper">
+//       {chips.map((chip) => (
+//         <a key={chip}>{chip}</a>
+//       ))}
+//     </div>
+//   );
+// }
+
 function BotTyping() {
   return (
-    <div className='message typing'>
-      <div className='dot'></div>
-      <div className='dot'></div>
-      <div className='dot'></div>
+    <div className="message typing">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
     </div>
   );
 }
 function Message({ rol, message }) {
-  return <span className={`message ${rol}-message`}>{message}</span>;
+  return (
+    <>
+      <div className={`message ${rol}-message`}>{message}</div>
+      {/* {chips && <Chips chips={chips} />} */}
+    </>
+  );
 }
