2e316be9825714c9ef890c119d7bd61cef76638d
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-05-28 14:35:56 -0500

    Add notification sound component

---
+++ src/components/NotificacionSound.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from 'react';
+
+const NOTIFICATION_SOUND = '../../public/notification-sound.mp3';
+
+export default function NotificationSound() {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    try {
+      audioRef.current.play();
+    } catch (error) {
+      console.log('Error reproduciendo el sonido de notificación')
+    }
+  }, []);
+
+  return <audio src={NOTIFICATION_SOUND} ref={audioRef}></audio>;
+}
